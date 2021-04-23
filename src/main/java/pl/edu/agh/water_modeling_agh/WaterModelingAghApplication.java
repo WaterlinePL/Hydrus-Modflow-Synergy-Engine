f0c62c510bed9cab80d14e08a5c3f5810fef7ce6
Maria Polak <bloqdymary@gmail.com>
2021-04-23 19:38:14 +0200

    Repo init

---
+++ src/main/java/pl/edu/agh/water_modeling_agh/WaterModelingAghApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.water_modeling_agh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterModelingAghApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterModelingAghApplication.class, args);
+	}
+
+}
