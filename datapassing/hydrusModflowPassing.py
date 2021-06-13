8ad5046386d837db9b9c93d2cab7900e13a842a7
Maria Polak <bloqdymary@gmail.com>
2021-06-13 15:30:52 +0200

    basic recharge update

--- datapassing/hydrusModflowPassing.py
+++ datapassing/hydrusModflowPassing.py
@@ -1,24 +1,33 @@
+import numpy as np
 import phydrus as ph
 import flopy
 import constants
 
-#TODO dowiedziec sie jakie parametry powinny tu być - prawdopodobnie shape nam dostarczy jakos info???
+
+# TODO dowiedziec sie jakie parametry powinny tu być - prawdopodobnie shape nam dostarczy jakos info???
 def updateRch():
     # read recharge value from T_LEVEL.out
     tLevel = ph.read.read_tlevel(path=constants.HYDRUS_ROOT + '\\T_LEVEL.out')
     rechargeValue = tLevel['sum(vBot)'].iat[-1]
 
-    # plug it into .rch and .dis (.dis is a Discretization File - it's required in all models and contains useful info)
-    ml = flopy.modflow.Modflow.load("simple1.nam", model_ws=constants.MODFLOW_ROOT, load_only=["rch", "dis"],
-                                    forgive=True)
+    # load MODFLOW model - basic info and RCH package
+    modflowModel = flopy.modflow.Modflow.load("simple1.nam", model_ws=constants.MODFLOW_ROOT, load_only=["rch"],
+                                              forgive=True)
 
-    print(ml.dis.nlay)  # layer count
-    print(ml.dis.nper)  # stress period count
-    print(ml.dis.nrow)  # rows count
-    print(ml.dis.ncol)  # cols count
+    # useful prop - modflowModel.nper (stress period count), modflowModel.nrow (rows), modflowModel.ncol (cols)
+    rchPackage = modflowModel.get_package("rch")  # get the RCH package
 
-    ml.rch.rech.array  # rch array 4d - [layer][stress_period][row][column]
-    # TODO - update .rch
+    # create new recharge array
+    rechargeArray = np.empty((modflowModel.nrow, modflowModel.ncol))
+    rechargeArray.fill(rechargeValue)  # TODO shapes handling
+
+    stressPeriod = 0  # stress period will always be 0 (based on our notes)
+    modflowModel.rch.rech[stressPeriod] = rechargeArray
+    newRecharge = modflowModel.rch.rech
+
+    # generate and save new RCH (same properties, different recharge)
+    flopy.modflow.ModflowRch(modflowModel, nrchop=rchPackage.nrchop, ipakcb=rchPackage.ipakcb, rech=newRecharge,
+                             irch=rchPackage.irch).write_file(check=False)
 
 
 def updateWodyGruntowe():
