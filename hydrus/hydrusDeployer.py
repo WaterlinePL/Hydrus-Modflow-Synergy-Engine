7e3759bbe51563019e6e62c2dae319c353b07bb6
Maria Polak <marias.polak@gmail.com>
2021-08-01 18:43:17 +0200

    Nic tu nie ma

--- hydrus/hydrusDeployer.py
+++ hydrus/hydrusDeployer.py
@@ -3,16 +3,15 @@ from kubernetes.client.rest import ApiException
 import yaml
 from os import path
 
-def deployModflow():
+
+def deployHydrus():
     config.load_kube_config()
     api_instance = client.CoreV1Api()
 
-
-    name = 'modflow-2005'
+    name = 'hydrus-1d'
     resp = None
     try:
-        resp = api_instance.read_namespaced_pod(name=name,
-                                                namespace='default')
+        resp = api_instance.read_namespaced_pod(name=name, namespace='default')
     except ApiException as e:
         if e.status != 404:
             print("Unknown error: %s" % e)
@@ -23,9 +22,7 @@ def deployModflow():
         with open(path.join(path.dirname(__file__), "config.yaml")) as f:
             pod_manifest = yaml.safe_load(f)
 
-        resp = api_instance.create_namespaced_pod(body=pod_manifest,
-                                                  namespace='default')
-
+        resp = api_instance.create_namespaced_pod(body=pod_manifest, namespace='default')
 
     print("Listing pods with their IPs:")
     ret = api_instance.list_pod_for_all_namespaces(watch=False)
--- server/main.py
+++ server/main.py
@@ -1,4 +1,23 @@
+import datapassing.hydrusModflowPassing as dataPasser
+from hydrus.hydrusDeployer import deployHydrus
+from modflow.modflowDeployer import deployModflow
+from kubernetes import client, config, watch
 
 if __name__ == '__main__':
-    print('PyCharm')
+    
+
+    api_instance = client.CoreV1Api()
+    count = 10
+    watch = watch.Watch()
+    for event in watch.stream(api_instance.list_namespace, _request_timeout=60):
+        print("Event: %s %s" % (event['type'], event['object'].metadata.name))
+        print(client.V1PodStatus().container_statuses)
+        count -= 1
+        if not count:
+            watch.stop()
+
+    deployHydrus()
+    dataPasser.updateRch()
+    deployModflow()
+
 
