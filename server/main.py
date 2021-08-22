da314bfcd9430fb7fade49e38ae8960d0887587f
FluffyNinjaBrick <jurek.j2505@gmail.com>
2021-08-22 15:16:52 +0200

    prepared bare-bones flask app

--- server/main.py
+++ server/main.py
@@ -1,4 +1,38 @@
 
-if __name__ == '__main__':
-    print('PyCharm')
+from flask import Flask, render_template
 
+app = Flask("App")
+
+all_posts = [
+    {
+        'title': "post 1",
+        'content': "this is content",
+        'author': "Marek Gajecki"
+    },
+    {
+        'title': "post 2",
+        'content': "this is also content"
+    }
+]
+
+
+# template rendering
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+# parameter passing and method limiting
+@app.route('/home/<string:name>', methods=['GET'])
+def hello(name):
+    return 'sup ' + name
+
+
+# template functionality showcase
+@app.route('/posts')
+def posts():
+    return render_template('posts.html', posts=all_posts)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
