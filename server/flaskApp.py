087d3fd0fa06e2592bcd838d54a4efa1122e450c
FluffyNinjaBrick <jurek.j2505@gmail.com>
2021-09-08 00:31:35 +0200

    implemented hydrus models upload; this stuff needs a cleanup

--- server/flaskApp.py
+++ server/flaskApp.py
@@ -3,7 +3,11 @@ from zipfile import ZipFile
 
 import os
 
+import constants
+
 app = Flask("App")
+
+# ------------------- EXAMPLES -------------------
 
 all_posts = [
     {
@@ -36,11 +40,31 @@ def posts():
     return render_template('posts.html', posts=all_posts)
 
 
+# ------------------- END EXAMPLES -------------------
 # archive uploading
 
 # this should ultimately go into a config file
-app.config["PROJECT_DIR"] = '../workspace'
-app.config["ALLOWED_TYPES"] = ["ZIP", "RAR", "7Z"]
+allowed_types = ["ZIP", "RAR", "7Z"]
+
+project_root = constants.PROJECT_ROOT
+workspace_dir = os.path.join(project_root, 'workspace')
+print(workspace_dir)
+modflow_dir = os.path.join(workspace_dir, 'modflow')
+hydrus_dir = os.path.join(workspace_dir, 'hydrus')
+
+loaded_hydrus_models = []  # an array of strings, the names of the loaded hydrus models
+
+
+# create the necessary folder structure
+def verify_dir_exists_or_create(path: str):
+    if not os.path.isdir(path):
+        print('Directory ' + path + ' does not exist, creating...')
+        os.system('mkdir ' + path)
+
+
+verify_dir_exists_or_create(workspace_dir)
+verify_dir_exists_or_create(modflow_dir)
+verify_dir_exists_or_create(hydrus_dir)
 
 
 def type_allowed(filename):
@@ -51,11 +75,11 @@ def type_allowed(filename):
 
     # check if it's allowed
     extension = filename.rsplit('.', 1)[1]
-    return extension.upper() in app.config["ALLOWED_TYPES"]
+    return extension.upper() in allowed_types
 
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
+@app.route('/upload-modflow', methods=['GET', 'POST'])
+def upload_modflow():
 
     # POST handler
     if request.method == 'POST' and request.files:
@@ -65,17 +89,54 @@ def upload():
         if type_allowed(project.filename):
 
             # save, unzip, remove archive
-            archive_path = os.path.join(app.config["PROJECT_DIR"], project.filename)
+            archive_path = os.path.join(workspace_dir, 'modflow', project.filename)
             project.save(archive_path)
             with ZipFile(archive_path, 'r') as archive:
-                archive.extractall(app.config["PROJECT_DIR"])
+                archive.extractall(os.path.join(workspace_dir, 'modflow'))
             os.remove(archive_path)
+
             print("Project uploaded successfully")
+            return redirect(request.root_url + 'upload-hydrus')
 
         else:
             print("Invalid archive format, must be one of: ", end='')
-            print(app.config["ALLOWED_TYPES"])
+            print(allowed_types)
+            return redirect(request.url)
 
-        return redirect(request.url)
+    return render_template('uploadModflow.html')
 
-    return render_template('upload.html')
+
+@app.route('/upload-hydrus', methods=['GET', 'POST'])
+def upload_hydrus():
+
+    # POST handler
+    if request.method == 'POST' and request.files:
+
+        project = request.files['archive-input']  # matches HTML input name
+
+        if type_allowed(project.filename):
+
+            # save, unzip, remove archive
+            archive_path = os.path.join(workspace_dir, 'hydrus', project.filename)
+            project.save(archive_path)
+            with ZipFile(archive_path, 'r') as archive:
+
+                # get the project name and remember it
+                project_name = project.filename.split('.')[0]
+                loaded_hydrus_models.append(project_name)
+
+                # create a dedicated catalogue and load the project into it
+                os.system('mkdir ' + os.path.join(workspace_dir, 'hydrus', project_name))
+                archive.extractall(os.path.join(workspace_dir, 'hydrus', project_name))
+
+            os.remove(archive_path)
+
+            print("Project uploaded successfully")
+            return redirect(request.root_url + 'upload-hydrus')
+
+        else:
+            print("Invalid archive format, must be one of: ", end='')
+            print(allowed_types)
+            return redirect(request.url)
+
+    return render_template('uploadHydrus.html', model_names=loaded_hydrus_models)
