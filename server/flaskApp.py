22c69e709ea92daa6f7d04458fe4a0438eae265b
FluffyNinjaBrick <jurek.j2505@gmail.com>
2021-09-11 15:04:29 +0200

    moved handler logic out of handler methods themselves

--- server/flaskApp.py
+++ server/flaskApp.py
@@ -9,6 +9,7 @@ util = AppUtils()
 util.setup()
 app = Flask("App")
 
+
 # ------------------- EXAMPLES -------------------
 
 all_posts = [
@@ -43,65 +44,77 @@ def posts():
 
 
 # ------------------- END EXAMPLES -------------------
+
+
+# ------------------- ROUTES -------------------
+
 @app.route('/upload-modflow', methods=['GET', 'POST'])
 def upload_modflow():
-
-    # POST handler
     if request.method == 'POST' and request.files:
-
-        project = request.files['archive-input']  # matches HTML input name
-
-        if util.type_allowed(project.filename):
-
-            # save, unzip, remove archive
-            archive_path = os.path.join(util.workspace_dir, 'modflow', project.filename)
-            project.save(archive_path)
-            with ZipFile(archive_path, 'r') as archive:
-                archive.extractall(os.path.join(util.workspace_dir, 'modflow'))
-            os.remove(archive_path)
-
-            print("Project uploaded successfully")
-            return redirect(request.root_url + 'upload-hydrus')
-
-        else:
-            print("Invalid archive format, must be one of: ", end='')
-            print(util.allowed_types)
-            return redirect(request.url)
-
-    return render_template('uploadModflow.html')
+        return upload_modflow_handler(request)
+    else:
+        return render_template('uploadModflow.html')
 
 
 @app.route('/upload-hydrus', methods=['GET', 'POST'])
 def upload_hydrus():
-
-    # POST handler
     if request.method == 'POST' and request.files:
+        return upload_hydrus_handler(request)
+    else:
+        return render_template('uploadHydrus.html', model_names=util.loaded_hydrus_models)
 
-        project = request.files['archive-input']  # matches HTML input name
+# ------------------- END ROUTES -------------------
 
-        if util.type_allowed(project.filename):
 
-            # save, unzip, remove archive
-            archive_path = os.path.join(util.workspace_dir, 'hydrus', project.filename)
-            project.save(archive_path)
-            with ZipFile(archive_path, 'r') as archive:
+# ------------------- HANDLERS -------------------
 
-                # get the project name and remember it
-                project_name = project.filename.split('.')[0]
-                util.loaded_hydrus_models.append(project_name)
+def upload_modflow_handler(req):
+    project = req.files['archive-input']  # matches HTML input name
 
-                # create a dedicated catalogue and load the project into it
-                os.system('mkdir ' + os.path.join(util.workspace_dir, 'hydrus', project_name))
-                archive.extractall(os.path.join(util.workspace_dir, 'hydrus', project_name))
+    if util.type_allowed(project.filename):
 
-            os.remove(archive_path)
+        # save, unzip, remove archive
+        archive_path = os.path.join(util.workspace_dir, 'modflow', project.filename)
+        project.save(archive_path)
+        with ZipFile(archive_path, 'r') as archive:
+            archive.extractall(os.path.join(util.workspace_dir, 'modflow'))
+        os.remove(archive_path)
 
-            print("Project uploaded successfully")
-            return redirect(request.root_url + 'upload-hydrus')
+        print("Project uploaded successfully")
+        return redirect(req.root_url + 'upload-hydrus')
 
-        else:
-            print("Invalid archive format, must be one of: ", end='')
-            print(util.allowed_types)
-            return redirect(request.url)
+    else:
+        print("Invalid archive format, must be one of: ", end='')
+        print(util.allowed_types)
+        return redirect(req.url)
 
-    return render_template('uploadHydrus.html', model_names=util.loaded_hydrus_models)
+
+def upload_hydrus_handler(req):
+    project = req.files['archive-input']  # matches HTML input name
+
+    if util.type_allowed(project.filename):
+
+        # save, unzip, remove archive
+        archive_path = os.path.join(util.workspace_dir, 'hydrus', project.filename)
+        project.save(archive_path)
+        with ZipFile(archive_path, 'r') as archive:
+
+            # get the project name and remember it
+            project_name = project.filename.split('.')[0]
+            util.loaded_hydrus_models.append(project_name)
+
+            # create a dedicated catalogue and load the project into it
+            os.system('mkdir ' + os.path.join(util.workspace_dir, 'hydrus', project_name))
+            archive.extractall(os.path.join(util.workspace_dir, 'hydrus', project_name))
+
+        os.remove(archive_path)
+
+        print("Project uploaded successfully")
+        return redirect(req.root_url + 'upload-hydrus')
+
+    else:
+        print("Invalid archive format, must be one of: ", end='')
+        print(util.allowed_types)
+        return redirect(req.url)
+
+# ------------------- END HANDLERS -------------------
