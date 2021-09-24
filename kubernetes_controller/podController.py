9c0e1b94665cd83d14a1c1dc1aa3271de8e19480
Maria Polak <bloqdymary@gmail.com>
2021-09-24 20:21:58 +0200

    clean up

--- kubernetes_controller/podController.py
+++ kubernetes_controller/podController.py
@@ -7,24 +7,30 @@ class PodController:
     def __init__(self, api_instance: client.CoreV1Api):
         self.api_instance = api_instance
 
-    def wait_for_pod_termination(self, pod_name: str):
+    def wait_for_pod_termination(self, pod_name: str) -> None:
         pod_watch = watch.Watch()
         for event in pod_watch.stream(self.api_instance.list_pod_for_all_namespaces, watch=True):
             o = event['object']
             if o.metadata.name == pod_name:
-                if o.status.container_statuses is not None and \
-                        o.status.container_statuses[0].state.terminated is not None:
-                    terminated = o.status.container_statuses[0].state.terminated
-                    if terminated.reason != 'Completed':
-                        raise Exception(pod_name + ' terminated unexpectedly. Reason: ' + terminated.reason)
-                    else:
-                        self.delete_pod(o.metadata.namespace, pod_name)
-                        pod_watch.stop()
+                if self.is_completed(o.status):
+                    self.delete_pod(o.metadata.namespace, pod_name)
+                    pod_watch.stop()
 
-    def delete_pod(self, namespace: str, pod_name: str):
+    def delete_pod(self, namespace: str, pod_name: str) -> None:
         print('Deleting pod ' + pod_name)
         try:
             self.api_instance.delete_namespaced_pod(pod_name, namespace)
             print('Pod deleted successfully')
         except ApiException as ex:
             print('Could not delete pod ' + pod_name + '.\n' + ex)
+
+    def is_completed(self, pod_status) -> bool:
+        if pod_status.container_statuses is None or \
+                pod_status.container_statuses[0].state.terminated is None:
+            return False
+
+        if pod_status.container_statuses[0].state.terminated.reason == 'Completed':
+            return True
+
+        raise Exception('Pod terminated unexpectedly. Reason: ' +
+                        pod_status.container_statuses[0].state.terminated.reason)
