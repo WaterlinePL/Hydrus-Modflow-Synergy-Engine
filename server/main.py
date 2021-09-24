80080d8a658087e011c53202d10cd8079d192f58
Maria Polak <bloqdymary@gmail.com>
2021-09-24 20:45:34 +0200

    clean up

--- server/main.py
+++ server/main.py
@@ -1,16 +1,7 @@
-# import datapassing.hydrusModflowPassing as dataPasser
-# from datapassing.hydrusModflowPassing import HydrusModflowPassing
-
-
-# def list_all_pods(api: client.CoreV1Api):
-#     print("Listing pods with their IPs:")
-#     ret = api.list_pod_for_all_namespaces(watch=False)
-#     for i in ret.items:
-#         print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
-
 from flaskApp import app
 
 if __name__ == '__main__':
+    
     # run flask app
     app.run(debug=True)
 
